5e061c8dfa16192ebb72b6ced43c045ad5587811
Pedro Henrique Silva Oliveira <tqi_phsoliveira@uolinc.com>
2021-03-18 22:14:32 -0300

    add funcion ordered integers

--- ordered_integers/main.go
+++ ordered_integers/main.go
@@ -1,9 +1,13 @@
 package main
 
 import "fmt"
+import "sort"
 
 func orderedIntegers(){
 	fmt.Println("Sorting integers...")
+	sorting := []int{4, 10, 2, 3, 1}
+	sort.Ints(sorting)
+	fmt.Println(sorting)
 }
 
 func main(){
