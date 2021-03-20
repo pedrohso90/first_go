077ba8d656cd4347f34dbeb61747661040f6b027
Jonas Cavalcanti <jonascavalcantineto@gmail.com>
2021-03-20 09:26:21 -0300

    Rafactoring

--- ordered_integers/main.go
+++ ordered_integers/main.go
@@ -1,18 +1,26 @@
 package main
 
-import "fmt"
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 // function ordered and bigger number
-func orderedIntegers(sorting[]int) {
-	sort.Ints(sorting)
-	fmt.Println("Sorting integers: ", sorting)
-	b := sorting[len(sorting)-1]
-	fmt.Println("Bigger number: ", b)
+func sortGetMaxArrayNumber(aSort []int) (int, []int) {
+	sort.Ints(aSort)
+	maxNumber := aSort[len(aSort)-1]
+	return maxNumber, aSort
 }
 
 // function main
-func main(){
-	numbers := []int{4, 10, 2, 3, 1} 
-	orderedIntegers(numbers)
+func main() {
+	numbers := []int{4, 10, 2, 3, 1}
+
+	var maxNumber int = 0
+
+	maxNumber, numbers = sortGetMaxArrayNumber(numbers)
+
+	fmt.Println("Integers sorted: ", numbers)
+
+	fmt.Println("Max number: ", maxNumber)
 }
