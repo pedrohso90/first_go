34b259a3a05d70a8f1be95511a83140821f3d428
Pedro Oliveira <pedrohso90@gmail.com>
2021-03-20 10:54:29 -0300

    fixing function echo

--- echo/main.go
+++ echo/main.go
@@ -7,8 +7,11 @@ import (
 )
 
 func main(){
+	// read arguments
 	p := os.Args[1:]
+	// array to string
     s := strings.Join(os.Args[1:], ", ")
+	// check if argument is empty
 	if len(os.Args) > 1 {
 		fmt.Println("array: ", p)
 		fmt.Println("string: ", s)
